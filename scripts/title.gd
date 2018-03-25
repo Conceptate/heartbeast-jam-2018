75b2f3da8489240f54c4b34c22ac225ccfe11c63
Toastyflame <toastyflamegames@gmail.com>
2018-03-25 17:19:42 -0600

    Add title screen
    scene switching will be added later

---
+++ scripts/title.gd
@@ -0,0 +1,9 @@
+extends Node
+#
+#func _ready():
+#	set_process(true)
+#	pass
+#
+#func _process():
+#	if (Input.is_action_pressed("title_transition")):
+#		SceneTree.change_scene("res://stages/game.tscn")
